1cdf703f363f2b6881044b09f9b8c1890dc40e32
jeanmbiz <jeanmbiz@hotmail.com>
2024-06-28 14:59:26 -0300

    feat: initial commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <body
+      style={{
+        margin: "0 auto",
+        backgroundColor: "pink",
+        color: "white",
+        textAlign: "center",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>teste de ammend ;)</h1>
+    </body>
+  );
+}
+
+export default Home;
